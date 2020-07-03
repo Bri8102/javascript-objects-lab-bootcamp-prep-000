b3d507b0a0f6ea2eb84a919855b7c695884308fa
Briana Oniru <damioniru16@gmail.com>
2020-07-03 19:49:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function updateObjectWithKeyAndValue(object, key, value ) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
-  obj[key] = value;
+  object[key] = value;
 }
